7a0e5056e2a731a39c82fa04c297ecba6388f902
tyetyetye <josh.tye@gmail.com>
2020-03-17 20:32:28 -0500

    Remapped rows to have names corresponding to table names

--- modules/investigate.py
+++ modules/investigate.py
@@ -14,7 +14,9 @@ class tt_investigate():
                     with contextlib.closing(conn.cursor()) as self.c:
                         unread_rows = self.get_unread()
                         #self.do_read()
-                        self.analysis(unread_rows)
+                        rows = self.rows_dict(unread_rows)
+                        print(rows)
+                        #self.process(rows)
 
     # Get all unread rows
     def get_unread(self):
@@ -27,6 +29,28 @@ class tt_investigate():
         sql_q = "UPDATE tt_log SET read = True WHERE read = FALSE"
         self.c.execute(sql_q)
 
-    def analysis(self, rows):
-        for row in rows:
-            print(row[2])
+    def rows_dict(self, rows):
+        # Dictionarize SQL rows
+        table_map = {0: 'id',
+                     1: 'datetime',
+                     2: 'filter',
+                     3: 'ether_src',
+                     4: 'ip_src',
+                     5: 'ip_dst',
+                     6: 'tcp_src',
+                     7: 'tcp_dst',
+                     8: 'read'
+                     }
+        sql_dict = {}
+        d_sql = {}
+        dd_sql = {}
+        for row in range(len(rows)):
+            sql_dict[row] = rows[row]
+            for items in range(len(sql_dict[row])):
+                a = table_map[items]
+                d_sql[a] = sql_dict[row][items]
+            sql_dict[row] = d_sql
+        return(sql_dict)
+
+
+
